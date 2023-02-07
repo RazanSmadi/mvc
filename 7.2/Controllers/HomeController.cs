3243572fd3764a99c8a4fa9e71a7f8fc9f4fb8fa
RazanSmadi <115452475+RazanSmadi@users.noreply.github.com>
2023-02-07 16:47:08 +0300

    /

---
+++ 7.2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using _7._2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _7._2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Entities2 db = new Entities2();
+        public ActionResult Index()
+        {
+            var emp = db.Services.ToList();
+            return View(emp);
+        }
+       
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 7.2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7._2.MvcApplication" Language="C#" %>
