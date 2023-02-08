b5e0f1b590eeca78f03e3fe419ca1d2696508292
RazanSmadi <115452475+RazanSmadi@users.noreply.github.com>
2023-02-08 15:52:10 +0300

    /*/

---
+++ Authontication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Authontication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authontication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        Entities2 db = new Entities2();
+       
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
